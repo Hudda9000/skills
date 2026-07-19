29b75ead9de098559902e2fc4e5ff5803b7ca195
Tom <thomasmarkwell@outlook.com>
2026-07-19 16:24:44 +1000

    fix: handle inquirer import compatibility

--- bin/index.js
+++ bin/index.js
@@ -1,7 +1,7 @@
 #!/usr/bin/env node
 
 const { program } = require('commander');
-const inquirer = require('inquirer');
+const inquirer = require('inquirer').default || require('inquirer');
 const chalk = require('chalk');
 const { execSync } = require('child_process');
 const path = require('path');
